9d82981d811981c33c2f5791292f49d134191489
Emperormoh <muhammedbello1005@gmail.com>
2021-11-11 08:37:19 +0100

    Assignmentusual

---
+++ ScoreListUpgrade/MarkUpgrader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace ScoreListUpgrade
+{
+    public class MarkUpgrader
+    {
+        public int Upgrade(int oldMark)
+        {
+            int newMark = 0;
+            if (oldMark > 50)
+            {
+                newMark = oldMark + 5;
+            }
+            else
+            {
+                newMark = oldMark + 10;
+            }
+            //int newMark = oldMark + 5;
+            //if (oldMark < 50)
+            //    newMark = oldMark + 10;
+
+            return newMark;
+
+
+
+
+
+        }
+    }
+}
---
+++ ScoreListUpgrade/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ScoreListUpgrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var oldScore = "ScoreLog.txt";
+            var upgradedScoreList = "newsccore.txt";
+
+
+
+
+
+            //File.Create(upgradedScoreList);
+            File.WriteAllText(upgradedScoreList, "");
+
+
+            foreach (var line in File.ReadAllLines(oldScore))
+            {
+                var dMark = line.Split("-")[1];
+                var markUp = new MarkUpgrader();
+                var newMark = markUp.Upgrade(Int32.Parse(dMark));
+                var newLineContent = line.Split("-")[0] + "-" + newMark;
+                File.AppendAllLines(upgradedScoreList, new string[] { newLineContent });
+            }
+
+
+
+
+
+    }   }
+}
